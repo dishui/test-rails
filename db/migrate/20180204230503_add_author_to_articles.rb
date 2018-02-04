5aa530fda44a4b979b4a78d2d6a0469107ec5490
Josephine Liang <josephine@MCHQ-50-MBP.local>
2018-02-04 15:24:07 -0800

    add article

---
+++ db/migrate/20180204230503_add_author_to_articles.rb
@@ -0,0 +1,5 @@
+class AddAuthorToArticles < ActiveRecord::Migration[5.1]
+  def change
+    #add_column :articles, :updated_at, :datetime
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180204202227) do
+ActiveRecord::Schema.define(version: 20180204230503) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "comments", force: :cascade do |t|
     t.integer "post_id"
